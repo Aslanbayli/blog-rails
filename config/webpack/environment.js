18814ae3bcd1d8b9e0d7ddab83a353303fc5ada8
Aslanbayli <aliaslanbayli@gmail.com>
2021-03-04 19:06:31 -0500

    initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,12 @@
+const { environment } = require('@rails/webpacker');
+
+// setting up newly added plugins
+const webpack = require('webpack');
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}));
+
+module.exports = environment;
